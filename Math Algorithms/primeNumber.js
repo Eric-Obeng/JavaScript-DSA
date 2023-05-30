a245f9b97646a04505acd944db583e3c67434f0b
Eric-Obeng <obengeric.3958@gmail.com>
2023-05-30 22:05:38 +0000

    isPrime

---
+++ Math Algorithms/primeNumber.js
@@ -0,0 +1,22 @@
+//Problem: Given a natural number 'n', determine if the
+//number is prime or not
+
+//A prime number is a natural number greater than 1 that is not 
+//a product of two smaller natural numbers
+
+function isPrime(n) {
+    if(n < 2) {
+        return false
+    }
+
+    for(let i = 2; i <= Math.sqrt(n); i++) {
+        if(n % i === 0) {
+            return false
+        }
+    }
+    return true
+}
+
+console.log(isPrime(2))
+
+//Big O = O(n)
