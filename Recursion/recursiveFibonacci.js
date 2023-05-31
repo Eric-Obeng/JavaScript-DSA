478779d8d4aad9cbd2effbd2fc5ec75317e61f6a
Eric-Obeng <obengeric.3958@gmail.com>
2023-05-31 13:09:44 +0000

    updated

--- Recursion/recursiveFibonacci.js
+++ Recursion/recursiveFibonacci.js
@@ -1,1 +1,11 @@
 //Problem: Given a number 'n' find the nth element of the FIbonacci sequence
+function recursiveFib(n) {
+    if(n < 2) {
+        return n
+    }
+    return recursiveFib(n-1) + recursiveFib(n-2) 
+}
+
+console.log(recursiveFib(6))
+
+//Big O = O(2*n)
