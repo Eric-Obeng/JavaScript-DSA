082c9b502298866f0b4063ad894049cfdb9f14d7
Eric-Obeng <obengeric.3958@gmail.com>
2023-06-01 13:52:21 +0000

    linear search

---
+++ Search Algorithm/linearSearch.js
@@ -0,0 +1,16 @@
+//Problem: Given an array of 'n' elements and a target element 't',
+//find the index of 't' in the array. Return -1 if the target element is not found
+
+// Example arr = [-5, 2, 10, 4, 6], t=10 -> should return 2
+//         arr = [-5, 2, 10, 4, 6], t=6 -> should return 4
+
+function linearSearch(arr, target) {
+    for(let i = 0; i < arr.length; i++)  {
+        if(arr[i] === target) {
+            return i
+        }
+    }
+    return -1
+}
+
+console.log(linearSearch([-5, 2, 10, 4, 6], 5))
