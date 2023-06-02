a9335e7a833f02d60ebc38ffb16e4c5460a30481
Eric-Obeng <obengeric.3958@gmail.com>
2023-06-02 10:59:27 +0000

    recursive binary search updated

--- Search Algorithm/recursiveBinary.js
+++ Search Algorithm/recursiveBinary.js
@@ -1,3 +1,15 @@
 //Problem: Given a sorted array of 'n' elements and a target element 't',
 //find the index of 't' in the array. Return -1 if the target element is not found.
 //Binary search only works for sorted array
+
+function recursiveBinarySearch(arr, target) {
+    return search(arr, target, leftIndex = 0, rightIndex = arr.length - 1)
+}
+
+function search(arr, target, leftIndex, rightIndex) {
+    if(leftIndex > rightIndex) {
+        return -1
+    }
+
+    let middleIndex = 
+}
