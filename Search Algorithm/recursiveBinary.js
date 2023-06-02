c54f7e783d55113467847ff7b03f6c08a393a245
Eric-Obeng <obengeric.3958@gmail.com>
2023-06-02 11:04:41 +0000

    update

--- Search Algorithm/recursiveBinary.js
+++ Search Algorithm/recursiveBinary.js
@@ -11,5 +11,16 @@ function search(arr, target, leftIndex, rightIndex) {
         return -1
     }
 
-    let middleIndex = 
+    let middleIndex = Math.floor((leftIndex + rightIndex) / 2)
+    if(target === arr[middleIndex]) {
+        return middleIndex
+    }
+
+    if(target < arr[middleIndex]) {
+        return search(arr, target, leftIndex, middleIndex -1)
+    } else {
+        return search(arr, target, middleIndex + 1, rightIndex)
+
+    }
 }
+
