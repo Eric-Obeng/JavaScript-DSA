3cb93cc78c7eb7f5cac5995fe12c83979c5f0508
Eric-Obeng <obengeric.3958@gmail.com>
2023-06-03 12:34:49 +0000

    array

---
+++ Array/array.js
@@ -0,0 +1,18 @@
+//Problem: Given the array arr, find and return two indices of the array 
+//that add up to weight or return -1 if there is no combination that add up to weight
+
+function findSum(arr, n) {
+    for(let i = 0; i < arr.length; i++) {
+        for(let j = 1; j < arr.length; j++) {
+            if(arr[i]+arr[j] === n) {
+                return [arr[i], arr[j]]
+            }
+        }
+    }
+
+    return -1
+}
+
+console.log(findSum([1, 2, 4, 5, 6, 9], 6))
+console.log(findSum([1, 2, 4, 5, 6, 9], 9))
+console.log(findSum([1, 2, 4, 5, 6, 9], 10))
