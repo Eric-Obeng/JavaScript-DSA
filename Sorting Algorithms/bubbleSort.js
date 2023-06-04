3f55132d8f997c569c1a9a0f82b496fa91a2c118
Eric-Obeng <obengeric.3958@gmail.com>
2023-06-04 22:34:41 +0000

    bubble sort updated

--- Sorting Algorithms/bubbleSort.js
+++ Sorting Algorithms/bubbleSort.js
@@ -3,15 +3,22 @@
 //bubbleSort(arr) => should return [-6, -2, 4, 8, 20]
 
 function bubbleSort(arr) {
-    for(let i = 0; i < arr.length; i++) {
-        if(arr[i] > arr[i - 1]) {
-            let temp = arr[i]
-            arr[i] = arr[i - 1]
-            arr[i - 1] = temp
+    let swapped
+    do {
+        swapped = false
+        for(let i = 0; i < arr.length - 1; i++) {
+            if(arr[i] > arr[i + 1]) {
+                let temp = arr[i]
+                arr[i] = arr[i + 1]
+                arr[i + 1] = temp
+                swapped = true
+            }
         }
-    }
+    } while (swapped)
 }
 
 const arr = [-6, 20, 8, -2, 4]
 bubbleSort(arr)
 console.log(arr)
+
+//Big O = O(n * 2)
