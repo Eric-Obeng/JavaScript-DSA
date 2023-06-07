d29c91040ed8729a78741473fe3a0ad1a9918542
Eric-Obeng <obengeric.3958@gmail.com>
2023-06-07 23:33:38 +0000

    quick sort

--- Sorting Algorithms/quickSort.js
+++ Sorting Algorithms/quickSort.js
@@ -1,5 +1,11 @@
 function quickSort(arr) {
+    let pivot = arr[arr.length -1]
+    let left = []
+    let right = []
 
+    for(let i = 0; i < arr.length - 1; i++) {
+        
+    }
 }
 
 const arr = [8, 20, -2, 4, -6]
